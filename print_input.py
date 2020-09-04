a49a7bbbcb1b957309d34fb8811a61eaa8a3df4a
SANTHOSH KUMAR D <santhoshkdhana@gmail.com>
2020-09-04 18:15:38 +0530

    Add files via upload

---
+++ print_input.py
@@ -0,0 +1,17 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+integer = int(input())
+# Read and save an integer, double, and String to your variables.
+double = float(input())
+string = input()
+
+# Print the sum of both integer variables on a new line.
+print(i+integer)
+print(d+double)
+print(s+string)
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
