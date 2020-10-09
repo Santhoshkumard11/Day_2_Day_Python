50a2d349fbd8c935d172b45e9e5a363958b8b7c5
SANTHOSH KUMAR D <santhoshkdhana@gmail.com>
2020-10-09 18:45:12 +0530

    Add files via upload

---
+++ str-Replcae.py
@@ -0,0 +1,16 @@
+# string = str(input())
+# str1 = str(input())
+# str2 = str(input())
+# print(string.replace(str1,str2))
+
+#num = int(input())
+row =1;ct=1
+
+for ctr in range(int(input())):
+    
+    for ctr1 in range(row):
+        print(ct,end=' ')
+        ct +=1
+    row  +=1
+    print("\n")
+     
